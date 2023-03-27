08ca2bb73bebb961e4da92ed83ba328149dc2528
georgeeset <georgeperfect4u@gmail.com>
2023-03-27 10:59:49 +0100

    adjusted ip address to new server

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,6 @@
 
 file { '/home/vagrant/.ssh/config':
   ensure  => present,
-  content => "Host 139552-web-01\n  Hostname 18.208.119.87\n
+  content => "Host 139552-web-01\n  Hostname 18.208.119.87\n User ubuntu
   	     IdentityFile ~/.ssh/school\n  BatchMode yes"
 }
