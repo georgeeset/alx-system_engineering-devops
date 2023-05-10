804b4fc57f816dd0110bed2e095896d1c22b43c4
georgeeset <georgeperfect4u@gmail.com>
2023-05-10 22:19:26 +0100

    search hot

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+ a function that queries the Reddit API and prints the titles of the first
+ 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Print the titles of the first 10 hot posts listed
+    for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/search.json".format(subreddit)
+    header = {
+        "User-Agent": "George Eset alx student",
+    }
+
+    param = {
+         "limit": "10",
+        "sort": "hot",
+        "q": "{}".format(subreddit)
+    }
+
+    response = requests.get(url, headers=header, params=param, allow_redirects=False)
+    if response.status_code != 200:
+        print (response.status_code)
+        return 0
+    data = response.json().get("data")
+    children = data.get('children')
+
+    for child in children:
+        print(child.get('data').get('title'))
