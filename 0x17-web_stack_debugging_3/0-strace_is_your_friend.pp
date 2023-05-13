d3a1c458840e0fd57ad584d73b8277cdb1260605
georgeeset <georgeperfect4u@gmail.com>
2023-05-13 09:22:50 +0100

    file path edited

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix 500 error on wordpress files
 
 exec { '500 error fix':
-     path    => '/usr/local/bin/:/bin/'
+     path    => '/bin/bash'
      command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
     }
