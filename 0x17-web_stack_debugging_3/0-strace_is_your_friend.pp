539961bdbe26c10b755add64abdbe8eddb798c90
georgeeset <georgeperfect4u@gmail.com>
2023-05-13 10:09:37 +0100

    path adjust

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #fix 500 error on apache worldpress web server
-exec { "correct spell error":
-  path    => ['/usr/env bin', '/usr/bin '/usr/sbin', '/bin'],
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+exec { "correct_spell_error":
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
