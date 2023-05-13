281240750f098ed240a236e91ea30a8cc7215299
georgeeset <georgeperfect4u@gmail.com>
2023-05-13 10:12:35 +0100

    compressed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,2 @@
 #fix 500 error on apache worldpress web server
-exec { "correct_spell_error":
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
