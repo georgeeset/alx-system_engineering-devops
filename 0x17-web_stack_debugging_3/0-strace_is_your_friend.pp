0ab9f32b10a579513563ab98056926d938fa2079
georgeeset <georgeperfect4u@gmail.com>
2023-05-13 09:33:41 +0100

    remove unused codes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,1 @@
-# fix 500 error on wordpress files
-
-exec { '500 error fix':
-     command => 'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    }
+exec {'sudo sed -i s/phpp/php/g /var/www/html/wp-settings.php'}
