f827ca0c988931a689d78d89433d3807393b0c1b
georgeeset <georgeperfect4u@gmail.com>
2023-05-13 09:56:22 +0100

    hard things

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,5 @@
 #fix 500 error on apache worldpress web server
-exec {'sed -i s/phpp/php/g /var/www/html/wp-settings.php'}
+exec { "correct spell error":
+  path    => ['/usr/bin', '/usr/sbin', '/bin'],
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+ }
