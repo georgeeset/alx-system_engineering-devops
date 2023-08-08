ad85076155a364e0cbd975d0bd6f2b0a64a17d0a
georgeeset <georgeperfect4u@gmail.com>
2023-08-08 19:41:01 +0100

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Write a function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+     returns the number of subscribers (not active users,
+     total subscribers) for a given subreddit.
+
+     Return information about the subreddit.
+     Data includes the subscriber count,
+     description, and header image.
+
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {
+        "User-Agent": "George Eset alx student"
+    }
+    response = requests.get(url, headers=header, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,13 @@
+### API ADVANCED
+___
+
+Implement api calls with reddit rest api
+
+*The Aim of this project*
+
+*How to use an API with pagination
+*How to parse JSON results from an API
+*How to make a rescursive API call
+*How to sort a direcotry by its value
+
+Reddit API is the great API to use for this practice
